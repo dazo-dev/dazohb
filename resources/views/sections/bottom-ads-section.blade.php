1cffd184a1ae3661a5be25cce43b10b2ef98a445
busybeecalibuso <80184156+busybeecalibuso@users.noreply.github.com>
2021-03-25 03:10:39 +0800

    change ads url path

--- resources/views/sections/bottom-ads-section.blade.php
+++ resources/views/sections/bottom-ads-section.blade.php
@@ -1,17 +1,34 @@
-<div class="bottom-ads-section">
-@php
-        $url = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
-    @endphp
-	<div class="col-sm-12 display-flex display-block-mb">
-		@foreach ($data['bottombanner'] as $result)
-		<div class="col-lg-4 col-sm-12" style="text-align:center;">
-			
-			<a href="//{{$result->b_link}}" target=_blank><img src="//localhost/dazoAdmin/public/uploads/banner-img/{{$result->b_img_path}}" style="width:300px;height:300px;"></a>
-			
-		</div>
-		@endforeach
-		
-	</div>
-</div>
-
-
+<div class="bottom-ads-section">
+
+@php
+
+        $url = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
+
+    @endphp
+
+	<div class="col-sm-12 display-flex display-block-mb">
+
+		@foreach ($data['bottombanner'] as $result)
+
+		<div class="col-lg-4 col-sm-12" style="text-align:center;">
+
+			
+
+			<a href="//{{$result->b_link}}" target=_blank><img src="{{$url}}dazohb2/public/uploads/banner-img/{{$result->b_img_path}}" style="width:300px;height:300px;"></a>
+
+			
+
+		</div>
+
+		@endforeach
+
+		
+
+	</div>
+
+</div>
+
+
+
+
+
